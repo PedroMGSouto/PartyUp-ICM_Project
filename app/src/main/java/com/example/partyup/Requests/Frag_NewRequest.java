6b43b9b09c7cb7a48eae21f604e99428d016e6f8
PedroMGSouto <pedromgsouto@ua.pt>
2020-11-22 00:55:51 +0000

    Database optimized and New Requests Database structured

--- app/src/main/java/com/example/partyup/Requests/Frag_NewRequest.java
+++ app/src/main/java/com/example/partyup/Requests/Frag_NewRequest.java
@@ -8,6 +8,7 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,10 +43,13 @@ public class Frag_NewRequest extends Fragment {
     final FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref;
     DatabaseReference ref2;
+    DatabaseReference ref3;
+    DatabaseReference ref4;
     private RecyclerView recyclerView;
     private CardView doRequest;
     private EditText partyn;
     private HashMap<String,String> requestData;
+    private HashMap<String,String> chatsData;
     SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
     SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
     SimpleDateFormat timeFormat2 = new SimpleDateFormat("HH:mm:ss");
@@ -86,7 +90,6 @@ public class Frag_NewRequest extends Fragment {
                     gameList.add(game.getValue().toString());
                 }
                 String[] gameListA = gameList.toArray(new String[gameList.size()]);
-                System.out.println("LEN"+gameListA.length);
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.select_dialog_item,gameListA);
                 actv.setThreshold(1);
                 actv.setAdapter(adapter);
@@ -100,13 +103,8 @@ public class Frag_NewRequest extends Fragment {
         });
 
         doRequest.setOnClickListener(v1 -> {
-            System.out.println("clicked");
             String gameText = actv.getText().toString();
             String partyText = partyn.getText().toString();
-            System.out.println("ACTV:"+gameText.length());
-            System.out.println("Partyn:"+partyText.length());
-
-
 
             if(gameText.isEmpty()|| partyText.isEmpty()){
                 Toast.makeText(getActivity(),"Please fill all fields!",Toast.LENGTH_SHORT).show();
@@ -128,14 +126,9 @@ public class Frag_NewRequest extends Fragment {
 
 
             date = new Date();
-            System.out.println("DATE+++++"+gameText);
-            System.out.println("DATE+++++"+"1");
-            System.out.println("DATE+++++"+partyText);
-            System.out.println("DATE+++++"+dateFormat.format(date));
-            System.out.println("DATE++++++"+timeFormat.format(date));
-            System.out.println(Frag_Login.mAuth.getCurrentUser().getEmail());
 
             requestData=new HashMap<>();
+            chatsData=new HashMap<>();
             requestData.put("game",gameText);
             requestData.put("messageDate",dateFormat.format(date));
             requestData.put("messageTime",timeFormat.format(date));
@@ -143,12 +136,60 @@ public class Frag_NewRequest extends Fragment {
             requestData.put("partySize",partyText);
             requestData.put("sentBy", Frag_Login.mAuth.getCurrentUser().getDisplayName());
 
-            String encrypt = Frag_Login.mAuth.getCurrentUser().getEmail()+timeFormat2.format(date);
-            String hash = new String(Hex.encodeHex(DigestUtils.md5(encrypt)));
+            String requestUID = Frag_Login.mAuth.getCurrentUser().getEmail()+timeFormat2.format(date);
+            String clientUID = Frag_Login.mAuth.getCurrentUser().getEmail();
+            String hash = new String(Hex.encodeHex(DigestUtils.md5(requestUID)));
+            String hash2 = new String(Hex.encodeHex(DigestUtils.md5(clientUID)));
             ref2 = database.getReference("Requests/"+hash);
+
+            chatsData.put("lastMessageSent","");
+            chatsData.put("more_properties","");
+            ref3 = database.getReference("Chats/"+hash);
+            ref4 = database.getReference("userChats");
+            HashMap<String,String> users = new HashMap<>();
+            HashMap<String,Object> chatuid = new HashMap<>();
+            users.put("0",hash2);
+
+            ref4.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if(snapshot.hasChild(hash2)){
+                        Log.e("Debug","I have found the child!");
+                        ref4 = database.getReference("userChats/"+hash2);
+                        ref4.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                long n =snapshot.getChildrenCount();
+                                Log.e("Debug","The child has: "+n+ " chats");
+                                chatuid.put(String.valueOf(n),hash);
+                                ref4.updateChildren(chatuid);
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Log.e("Cancel","Cancelled1");
+                            }
+                        });
+                    }
+                    else{
+                        Log.e("Debug","I didn't found any child!");
+                        chatuid.put("0",hash);
+                        ref4 = database.getReference("userChats/"+hash2);
+                        ref4.setValue(chatuid);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.e("Cancel","Cancelled2");
+                }
+            });
+
 
             try {
                 ref2.setValue(requestData);
+                ref3.setValue(chatsData);
+                ref3.child("users").setValue(users);
             }
 
             catch (Exception ex){
